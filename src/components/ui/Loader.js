06a62c92e8cdffcbd6dd2245393bb4af171b3e5d
Bayron David <93720978+BayronDavid@users.noreply.github.com>
2025-03-17 20:34:50 -0500

    [feat][variantes de loader]

--- src/components/ui/Loader.js
+++ src/components/ui/Loader.js
@@ -1,25 +1,47 @@
 'use client';
-
 import React from 'react';
 import './Loader.css';
 
-export default function Loader({ size = 'medium', fullScreen = false, text }) {
+export default function Loader({
+  variant = 'default', // 'default' | 'donut' | 'flower'
+  size = 'medium',    // 'small' | 'medium' | 'large'
+  fullScreen = false,
+  text,
+}) {
   const getLoaderClass = () => {
     let className = 'loader';
-    
     if (size === 'small') className += ' loader-small';
     else if (size === 'large') className += ' loader-large';
     else className += ' loader-medium';
-    
     if (fullScreen) className += ' loader-fullscreen';
-    
     return className;
   };
-  
+
+  const renderSpinner = () => {
+    switch (variant) {
+      case 'donut':
+        return <div className="loader-spinner donut"></div>;
+      case 'flower':
+        return (
+          <div className="flower-loader">
+            <div className="petal petal1"></div>
+            <div className="petal petal2"></div>
+            <div className="petal petal3"></div>
+            <div className="petal petal4"></div>
+            <div className="petal petal5"></div>
+            <div className="petal petal6"></div>
+          </div>
+        );
+      case 'default':
+      default:
+        return <div className="loader-spinner"></div>;
+    }
+  };
+
   return (
     <div className={fullScreen ? 'loader-container' : ''}>
       <div className={getLoaderClass()}>
-        <div className="loader-spinner"></div>
+        {renderSpinner()}
         {text && <p className="loader-text">{text}</p>}
       </div>
     </div>
